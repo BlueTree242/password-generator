efd2a2d93571b1754ef16053c015f0e42b133840
BlueTree242 <yahiamsm2@gmail.com>
2022-04-02 13:37:06 +0200

    add my credit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,15 @@ export default function Footer() {
     return (
         <footer className="flex items-center justify-between">
             <DarkModeToggler />
-            <div className="text-slate-700 dark:text-slate-400">
+            <div className="text-slate-700 dark:text-slate-400 text-center justify-center">
                 made with <span className="text-red-600">&#10084;</span> by{" "}
                 <a href="https://web.facebook.com/yande.cc/" target="_blank" rel="noreferrer" className="font-bold">
                     Yande Arta
+                </a>
+                <br/>
+                Modified & Translated by{" "}
+                <a href="https://github.com/BlueTree242" target="_blank" rel="noreferrer" className="font-bold">
+                    BlueTree242
                 </a>
             </div>
         </footer>
